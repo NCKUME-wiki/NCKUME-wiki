0f7f72fdd4921de409c6791d7c2ecf792b8e1b7e
Ai-yung Chen <whu970038@gmail.com>
2025-02-08 00:41:06 +0800

    i don't know wtf

--- scripts/netlify/install-python.sh
+++ scripts/netlify/install-python.sh
@@ -82,6 +82,7 @@ for DIR in "${APT_PREFIX_COPY_DIRS[@]}"; do
  cp -r "$DIR/." "$APT_PREFIX$DIR/" || true;
 done
 for DIR in "${APT_PREFIX_COPY_FILES[@]}"; do cp -r "$DIR" "$APT_PREFIX$DIR" || true; done
+echo "im fine"
 cat /etc/apt/sources.list | grep 'ubuntu\.com' > "$APT_PREFIX/etc/apt/sources.list"
 rm -rf "$APT_PREFIX/etc/apt/sources.list.d"/*
 
