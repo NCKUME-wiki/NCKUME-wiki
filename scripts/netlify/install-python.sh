0bf59af7facf63456f2053f115ff8eac973cc11b
Ai-yung Chen <whu970038@gmail.com>
2025-02-08 00:54:53 +0800

    ubuntu source moved

--- scripts/netlify/install-python.sh
+++ scripts/netlify/install-python.sh
@@ -83,8 +83,12 @@ for DIR in "${APT_PREFIX_MAKE_DIRS[@]}"; do mkdir -p "$APT_PREFIX$DIR"; done
 for DIR in "${APT_PREFIX_COPY_DIRS[@]}"; do rsync -av "$DIR/." "$APT_PREFIX$DIR/" || true; done
 for DIR in "${APT_PREFIX_COPY_FILES[@]}"; do rsync -av "$DIR" "$APT_PREFIX$DIR" || true; done
 echo "copy done"
-cat /etc/apt/sources.list
-cat /etc/apt/sources.list | grep 'ubuntu\.com' > "$APT_PREFIX/etc/apt/sources.list"
+# cat /etc/apt/sources.list :
+# Ubuntu sources have moved to the /etc/apt/sources.list.d/ubuntu.sources
+# file, which uses the deb822 format. Use deb822-formatted .sources files
+# to manage package sources in the /etc/apt/sources.list.d/ directory.
+# See the sources.list(5) manual page for details.
+cat /etc/apt/sources.list.d/ubuntu.sources | grep 'ubuntu\.com' > "$APT_PREFIX/etc/apt/sources.list"
 echo "cat done"
 rm -rf "$APT_PREFIX/etc/apt/sources.list.d"/*
 echo "rm done"
